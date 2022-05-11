3f4b9f333568d71d68d9bfbc658616b6548e1bb8
Luar RaNa <luar.rana@protonmail.com>
2022-05-11 16:29:23 -0500

    Update Wezterm config

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,17 +1,26 @@
 local wezterm = require 'wezterm';
 
 return {
+  -- Cursor
   default_cursor_style = "BlinkingBlock",
+  force_reverse_video_cursor = true,
+  -- Font Conf
   font = wezterm.font({
     family = "JetBrainsMono Nerd Font Mono",
     -- harfbuzz_features = {"calt=0", "clig=0", "liga=0"},
   }),
-  font_size = 16,
-  initial_cols = 140,
-  initial_rows = 40,
-  -- keys = {
-  --   {key="f", mods="CTRL|CMD", action="ToggleFullScreen"},
-  -- }
+  font_size = 13,
+  line_height = 1.0,
+  -- Shorcuts
+  keys = {
+    {key="f", mods="CTRL|CMD", action="ToggleFullScreen"},
+  },
+  -- Window size and decorations
+  color_scheme = "Gruvbox Dark",
+  initial_cols = 200,
+  initial_rows = 50,
+  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
   window_decorations = "NONE",
   window_padding = {
     left = 0,
