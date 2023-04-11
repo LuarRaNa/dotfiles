ca7454bcc8d5012a98d7089f0ed5d6014178c75f
Luar RaNa <luar.rana@protonmail.com>
2023-04-10 21:47:56 -0600

    Update Wezterm config

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,31 +1,89 @@
 local wezterm = require 'wezterm';
 
 return {
-  default_prog = { '/opt/homebrew/bin/nu', '-l' },
-  -- Cursor
-  force_reverse_video_cursor = true,
+  default_prog = {
+    '/opt/homebrew/bin/nu',
+    '-l'
+  },
   -- Font Conf
-  font = wezterm.font({
-    family = "JetBrainsMono Nerd Font Mono",
-  }),
-  font_size = 13,
-  line_height = 1.0,
-  freetype_load_target = 'Light',
+  font = wezterm.font_with_fallback {
+    {
+      family = "IosevkaTerm Nerd Font",
+    }
+  },
+  font_size = 16,
   -- Shorcuts
   keys = {
-    {key="f", mods="CTRL|CMD", action=wezterm.action.ToggleFullScreen},
+    {
+      key = "f",
+      mods = "CTRL|CMD",
+      action = wezterm.action.ToggleFullScreen
+    },
   },
-  -- Window size and decorations
-  color_scheme = "tokyonight-storm",
-  initial_cols = 200,
+  -- Window
+  initial_cols = 150,
   initial_rows = 50,
   use_fancy_tab_bar = false,
   tab_bar_at_bottom = true,
   native_macos_fullscreen_mode = true,
+  window_close_confirmation = 'NeverPrompt',
   window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
+  },
+  colors = {
+    -- Rose Pine theme
+    foreground = "#575279",
+    background = "#faf4ed",
+    cursor_fg = "#575279",
+    cursor_bg = "#cecacd",
+    selection_fg = "#dfdad9",
+    selection_bg = "#575279",
+    brights = {
+      "#9893a5",
+      "#b4637a",
+      "#286983",
+      "#ea9d34",
+      "#56949f",
+      "#907aa9",
+      "#d7827e",
+      "#575279",
+    },
+    ansi = {
+      "#f2e9e1",
+      "#b4637a",
+      "#286983",
+      "#ea9d34",
+      "#56949f",
+      "#907aa9",
+      "#d7827e",
+      "#575279",
+    },
+    tab_bar = {
+      background = "#faf4ed",
+      active_tab = {
+        bg_color = "#faf4ed",
+        fg_color = "#575279",
+      },
+      inactive_tab = {
+        bg_color = "#faf4ed",
+        fg_color = "#9893a5",
+      },
+      inactive_tab_hover = {
+        bg_color = "#faf4ed",
+        fg_color = "#575279",
+      },
+      new_tab = {
+        bg_color = "#faf4ed",
+        fg_color = "#9893a5",
+      },
+      new_tab_hover = {
+        bg_color = "#faf4ed",
+        fg_color = "#575279",
+      },
+      inactive_tab_edge = "#9893a5",
+    },
   },
 }
