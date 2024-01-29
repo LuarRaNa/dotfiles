af1221f31fb82cc1d6d0bc72eed45a015e90d6db
Luar RaNa <luar.rana@protonmail.com>
2024-01-28 22:02:51 -0600

    Update Wezterm config
    - User dark themes

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,84 +1,4 @@
--- Theme Configuration
--- Rose Pine Dawn, from official website
-
-local palette = {
-    base = "#faf4ed",
-    surface = "#fffaf3",
-    overlay = "#f2e9e1",
-    muted = "#9893a5",
-    subtle = "#797593",
-    text = "#575279",
-    love = "#b4637a",
-    gold = "#ea9d34",
-    rose = "#d7827e",
-    pine = "#286983",
-    foam = "#56949f",
-    iris = "#907aa9",
-    highlight_low = "#f4ede8",
-    highlight_med = "#dfdad9",
-    highlight_high = "#cecacd",
-}
-
-local active_tab = {
-    bg_color = palette.overlay,
-    fg_color = palette.text,
-}
-
-local inactive_tab = {
-    bg_color = palette.base,
-    fg_color = palette.muted,
-}
-
-function colors()
-    return {
-        foreground = palette.text,
-        background = palette.base,
-        cursor_bg = palette.muted,
-        cursor_border = palette.muted,
-        cursor_fg = palette.text,
-        selection_bg = palette.overlay,
-        selection_fg = palette.text,
-        ansi = {
-            palette.surface,
-            palette.love,
-            palette.pine,
-            palette.gold,
-            palette.foam,
-            palette.iris,
-            palette.rose,
-            palette.text
-        },
-        brights = {
-            palette.subtle,
-            palette.love,
-            palette.pine,
-            palette.gold,
-            palette.foam,
-            palette.iris,
-            palette.rose,
-            palette.text
-        },
-        tab_bar = {
-            background = palette.base,
-            active_tab = active_tab,
-            inactive_tab = inactive_tab,
-            inactive_tab_hover = active_tab,
-            new_tab = inactive_tab,
-            new_tab_hover = active_tab,
-        }
-    }
-end
-
--- Configuration
-
 local wezterm = require "wezterm"
-local mux = wezterm.mux
-
-wezterm.on('gui-startup', function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    pane:split { size = 32 }
-    pane:split { size = 100 }
-end)
 
 return {
     front_end = "WebGpu",
@@ -91,9 +11,9 @@ return {
         { family = "Iosevka Fixed" },
         { family = "Symbols Nerd Font Mono", scale = 0.6 },
     },
-    font_size = 15,
-    initial_cols = 166,
-    initial_rows = 47,
+    font_size = 13,
+    initial_cols = 186,
+    initial_rows = 54,
     window_padding = {
         left = "0.5cell",
         right = "0.5cell",
@@ -104,21 +24,21 @@ return {
     enable_tab_bar = false,
     native_macos_fullscreen_mode = true,
     window_close_confirmation = "NeverPrompt",
-    colors = colors(),
+    color_scheme = 'AdventureTime',
     keys = {
         {
-            mods = 'CMD',
+            mods = 'CMD|SHIFT',
             key = 't',
             action = wezterm.action_callback(function(window, pane)
                 local mux = window:mux_window()
-                local current_wd = pane:get_current_working_dir()
+                local url = pane:get_current_working_dir()
                 local new_tab, new_pane, new_window = mux:spawn_tab {
                     cwd = wezterm.home_dir
                 }
-                new_pane:split { size = 32 }
+                new_pane:split { size = 41 }
                 new_pane:split {
                     size = 100,
-                    cwd = string.gsub(current_wd, 'file://', '')
+                    cwd = url.file_path
                 }
             end),
         },
