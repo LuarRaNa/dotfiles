852a3753b5566d3680cdfeff5b091d5275e4538f
Luar RaNa <luar.rana@protonmail.com>
2024-12-30 00:36:20 -0600

    Add Neovim config

---
+++ home/private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,19 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
